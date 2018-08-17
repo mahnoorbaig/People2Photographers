bf447eab475f2e1db03a1cc5716682cac0bb24b7
mahnoorbaig <mahnoorbaig.contact@gmail.com>
2018-08-17 16:37:34 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,184 @@
+var nearNorthSide = {
+  name: "Near North Side",
+  place1:
+    "Chicago RiverWalk: Enjoy a scenic walk or a lovely boat ride through the dazzling river!",
+  place2: "This place offers a fun backdrop to all your Instagram needs.",
+  place3:
+    "Merchandise Mart: Come take fun candids in the world’s largest office complex.",
+  person1name: "Niveda Tennety",
+  person1imageURL:
+    "https://lh3.googleusercontent.com/RxfiQz9686gQCBmtS5arMs1t5MXekXTU4r9oYcljhGDsJnUZ7Lz_VK-0yu4V1SfKiLuGRWpqIL8poNBPQtOTtmZxC5rrtHcIG9VsT0uP5zsnyO-JTuv7FLvLEQXeQdtdpVjNDTA631lJEWCzksu7K8k3atK7JNlYxk09e_tZ42-YrnrdxtQy3tm-EF0pbpKDYu9CABD2pCh7DKkDayeb_JY-10iDdUPztyVJeBjAgzLGGK8NrUzM8tYxMaHr4XqcapXufUcOCcd-KcgdOuxVh0WbC3Z26_ehOHVuqOY1vEPcCNmWj0r_02Sf9qgT3iNV0b0p7XAIccVc6gVwbb-Qdnt2DiMc9hLntSO9sl9ecjnmEdY0ZUK9YWQs-_ISLx6JRYAus-xa_B4ncPH_-EoCSHA8Mc7GmRymuOl3rF8CRhLEkxS0AirtDf3hP4TQyA_QEP04MKLHuwnxb8RQurm-igrN_PBmfZHGpOG4aniaPbBr5gUWUqFnEG_LmYVA2AmtkY5mdNTn3_jgsG6cQse1GfCuIGmLuqI9kyseTiBVvRlJlYRFQLracwIJvaBJZoTMnAYjs5h1PwPhGtXMjQmYhHv6y7cAhybFIIJ_Hwn8POxtKHxuez6mu-xN8Vibvw-MJZG5nxLxNprMRraNiuTEKDcnw5M-kMUw=w1534-h1034-no",
+  person1headline: "Lover of all things photography",
+  person2name: "Yeng Kwok",
+  person2imageURL:
+    "https://lh3.googleusercontent.com/NDIn_P3NwfUNPoc8fnE_0puIhlDkN72O4Q0we-SuU4joyM0LLcVTgjkSwYD54gs6-oth0pwWulUEDANuruVRPcZLueAPgpqB3hNlpIpPWGUnHXQnishXew4wXgYZXdGwa-IxG86pClwpak6hPi_lhOxvR67-MDmSdLrLGmfPwl5lQV6EFfFQqQ0hHYVKW2kPVJbag0V84z8oAwdzbcqdFO5MvtJLd5bbhFrgRkElB2eUISz23lqqEaSQgKgCxIrdZp6eynZIPaXmEwv0bdKPUsje5-382ty17c4VisyF1iqynGE6Ou-ONkeyHZx_BHVm8YOQWuvRhp06a9Z79NMBJeGrJruOp6bcoVGCC12A9dVR8XYukWVLeAs2Q5A04Hc5yZXLJkceVpiS0fkoISICIYm6hFi6-xKm5XfZgKyuf_OaBCMDTzugj2Rq88vIGsiR80BtQ0TW6kAny3YccGcjHRWKcDi7sEHF9UubAjN0lakJr6eaD0w2sTXVH0-JxJWyomtieIhklpmdIoHVv3IBIWGZ29L1zrRl2jLDuWLl4wij0J3SB5-_IaJv6hVJqsRWkxkUvlTSP3CIT-Pmg5Lm9O56q475A8ypZ3xQd9U=w2222-h1480-no",
+  person2headline: "IPHONE ONLY | NY | CHICAGO | LA",
+  file1: "Niveda.html",
+  file2: "yeng.html"
+};
+
+var chicagoLoop = {
+  name: "Chicago Loop",
+  place1:
+    "Millennium Park: “The Bean” sculpture offers tons of fun and creative photo-ops. This spot is a must visit!",
+  place2:
+    "Willis Tower: Enjoy 360 degree views spanning up to 50 miles and get your picture taken from the highest observatory in Chicago",
+  place3:
+    "Art Institute of Chicago: Enjoy with the impressive art featuring world class paintings and sculptures at this place",
+  person1name: "Nicole Xia",
+  person1imageURL:
+    "https://lh3.googleusercontent.com/JlpcEKVnzPB9DH4aiqHUoeLIklokpQwpjY1_YMioiRMFPIIq5sUvLwkfZtK7lJUgCfSlCPCTIU5RAlmX3f4KLO0Y7ILlWlxea_RfqFcQ1ixeAJ0_Ux3Mvog5gZVgMBgqmGo2kBu86yH4pBGUKN-8sUKT3yMba75yT-_pJdfQHBKZ4NhPNonVTRLQRAu0fIEd-Kx5Rj9mhPIDXB3ziDLbLGpcY-jzf7eOiUerzyrrpoMzWMCcco8E2P_akyyZqaie2ClSrbWkAe20pcYkw6Yb-A37yoPqwo7EwQxeBjAop2se2iQyBYimiRdprqyhjEKXCAu1hOEB_XVZM17cDwaFhIFE-6f0iFL2EDvniPTHej10FW2p-7L4go28WHRDQya2xeAvIoHDCehxINX3hOtx0lQY0CapSZP5tElnY3p_gIv1OtbHK0YCGRmol4EPS2xjWXhRCAx6heevsOd-ezNrazB0KSSASiDhsN9Icd9jpWkdF27F7x8vexcDUO9elKK0_65eOcEXj-5wd0r7_liBzt7NLea8LqVOC-6KQ5UKGE-AN8l1XjEJUDeUXn3uWQEOiwUv0oyZhKAthge3IvpCKkWgR2IfsVsx3lf-fZA=w2222-h1480-no",
+  person1headline: "Capturing the things I love, one moment at a time",
+  person2name: "Alice Liu",
+  person2imageURL:
+    "https://lh3.googleusercontent.com/__dVNNbj-1pKcQPAWD4UhoY_WamwVie-5IoH6TcCuyNytJag7l-5Tx-b2ozBHT34vd4hfQEXRFMyZTusIiBeBnriJKL5p7ejMQkJWgPxW9hiJAKzY4FWzSWOlzvyrx5dwKBjsyS-BRENY3ZRcUrgpDvfrKa6cfMc2yTAq7WllS0xDNPmztpaxcdTX91lOVUZktN22m1jURgs7jl0QjCgg-WzmLc_lsH1EJdsnZaMFdAcDfp_gZpkQvqjnjEI5Q6O8QDFWIIVVAqHaodme_EUR6FP1yl4ZWt1aW-mGI8YhYVx-d2pPqHFfA3R9rR01LxtOLz_M846Q_BG4XMiXxCNkoq-6HxMg-Wb67OEO4vO5FqtQv71DjEPY9zUGoyz-6EuJ4RaFPHRYiJwm-QHFEpmcI0Hee_vn5yNYPXKyC932s6quQTxFJer0Ue3rR1Z0iOwdiOfcnl9JUNjrgzCPoQU_Jj-gDOthYALFFgmviNUwTkwu3-2jzDUJdHi0GmcmLThPnczCOBHA3NZyBGtJQzebTMYZHhXnOakZpCtsx1DfPONrPCw-JzfVAStx8Md0ROmnBM5X3ORIKqCBxGohXunodaet1ayS439oZA9pOw=w2420-h1612-no",
+  person2headline:
+    "CS student at the morning, professional photographer at night",
+  file1: "nicole.html",
+  file2: "alice.html"
+};
+
+var nearSouthSide = {
+  name: "Near South Side",
+  place1:
+    "UChicago: Pretend to be a student in a research-facilitated university while enjoying the beauty of a world-renowned university’s architecture",
+  place2:
+    "House of Blues: Watch world’s top performers in a fun and upbeat setting",
+  place3:
+    "Museum of Contemporary Art: Explore the unique art from all the world all in one museum!",
+  person1name: "Eric Carreon",
+  person1imageURL:
+    "https://lh3.googleusercontent.com/fZHbKABVQchXeSO6uLiazHPcLPq4Fr5Owe9Nk1lzP-7RIfHm4xHyc4NHRPgWJfz5CKbK2kdu4XG9h5cJjfkB26K3-ejbZbC71_PnRtI038sZyDkNTTj3urTvy3oPIbbclqI-s0hEUDupHYlZK3sB4kqST82-8qd4DWmPAWRsq0eMhh5hWUmGaJpioDGp8YQR4T_tzXd3KJ0B2pkbG_E13mXdpGYwZUR9Z7WqeEpRh2Heab4M49ot7lpSDf4DI-8rIm8veLjJTlxfhxmff7XYUzoMM1qdVTR7Tob5xATHvEDXCJaPKoywB-dzDhBC1GUFZ9WqfCuo64G7KbJ2C_U8pa88xPN-aIZbVqA-0MlLONQ5re_O3PCobz4DRFSSB4VfOKlmfC9mY3W3egps0oVrQhGOjPGr7z35sS_LWsg5tohjCyRCUCnOQJeljDMWuS1VuYGEdaBPjh-Tpwk4a2tQs6rgQcaXStBteMn2C9eVm0wNRNUYEFjIIc_jNIIqUDV8KfhtNcI85d1Wf-qGHjjyPJyBg0TTVeccKH8P5nV72t-xTXWbm_mDimOej0lsEZXBuxJ3trizL9mT-5DyiLPftm4lAB2GGTU54Gq10a8=s1071-no",
+  person1headline:
+    "There is one thing the photograph must contain, the humanity of the moment.",
+  person2name: "Sylvia Peng",
+  person2imageURL:
+    "https://lh3.googleusercontent.com/WBeHyEYx-2uPReDvencPvvnjRgs7hYkTwbE5jm_PXUVEoAoQrJ4NWa3n3WcIiHRDz-jMtZkdMhvChlywv9ov6setM9rESUJoYwKIX02SryP1NbFisHa8zFbhRUMJ7rCQ7x8VxFoQoTpLZELq_n2pUW4eUesmQqLLo58M96sc7dHOi6MEDNSAO09YDvmoQh_1xbhWLAqfL9pvFaSk1xhpKPnrR7ja_qXH-nzOiQMrWCPzolq-7i0vWvNoFsCEESYH3Vn2ANQm3l5hmrmQ_pcCpeC2CQqJR73N1vCCYYORC9qvz08tiCyAftIsUEWhMUnfFTS-gpUzEwrNIzlDAKRLcIJqNYDD3Gr06yvkrCngUl_S4sWQg0kpxKMJSpHuPlvWA-4-_F4TJ927L6MKCVzjF06V9mlSni1_RA26shRl-WrNn_ltmEV2ciswmkKvAlOaaAmena2mdiaRMbdgp2_uBwRXCDbV-WboMx7PC108jNGbf8eeAIvphw4Kh0TroAo-3lrqbG-dsawkKiT8eSkrvpfTSOlC5L0IFGn7HjKYDk6IfPfuDzLS4XzozGmzTHCkTKj01A7xZ1oRd89XMFYwN8HsAJ6ONlYHcuwx9a_6FjVvEm--fPqDG5SHzu_kMjtRO95LBXYjQog64qFhtEXU8_T6FANiAE4t=w2420-h1612-no",
+  person2headline: "Photography is a way of feeling, of touching, of loving.",
+  file1: "eric.html",
+  file2: "sylvia.html"
+};
+
+var nearWestSide = {
+  name: "Near West Side",
+  place1:
+    "Vertiport Chicago VIP Helicopter Tours: Explore the city from the sky and see all the beauty of Chicago in the air",
+  place2:
+    "Girl and the Goat: This restaurant offers all kinds of aesthetic food and photo-ops",
+  place3: "Hoi: This Vietnamese restaurant is truly a hidden gem of Chicago",
+  person1name: "Anthony Pham",
+  person1imageURL:
+    "https://lh3.googleusercontent.com/pcq9gpWUzRerodwEK7_4UkFWh4ghJzrKYcpaYw0rlUbiA97lMXBJJVPAE5uxdv-z9MnE_G4y5N2kKwc2F6iixyZRHOAn9gLhB2bO4lJ_k0oMFfSab81AMVyAEPBg9FmvP2qi_ppqP_nzV6cfC4-jBTztR1S5Wx8In3YA9E5xkUCOervC-_LJb36LJE-_QpUKraPGBzOFKEcI4slxXQGMLFvkJR8MHlCLdlqAhs9HK7L9X2CFKgHQLFJseDFGheoVmoMoTarOAyHTroGoCYUkFxUiWb9V21J82gOV7CKFe0ZQfBGvDK-P32Qk0Zpj-7Fe2qqDK78p9rd5Z3oDfZ4cgFTjiOmgTssqjbFZqd1Z9GqVRo0KlcElUdeM5lRqb90CYBzZdGYK3hpRHO6HDQ1ScXXb2oaU2Pft2UnnXBaCMkzxf83N5FspkLXr99LeCmGiPc6pm7A_webIpFUipX9DpdjmYMZG6gtubDYCQEDwxsR5NxB-3LSMoL7Y_k7Hs91bRjlTve_-1dsyUxdF6k6Bh7HyvZDl__qBbl2o5DHuxFfPulbDQyzEfrW02vj9uybleFUN5H3dMqJUfrxrwbPIgT4Mrz908mfBQOvNIlKnnW7uaDIUcCZlcuLfFm4PI1AGqQE6Wyz2sf6H7AFttHX73-AT3QwiqsvW=w2416-h1612-no",
+  person1headline: "I love my life just the way I capture my photos",
+  person2name: "Diane Wei",
+  person2imageURL:
+    "https://lh3.googleusercontent.com/Xe9ZsvjMyMhfoSqbJcbBq1v5NQoJusF4MvML-xWWM8u__NC1FRlZaY5g5mH2zGMrNR6HBv7r0bsc6MxPhNaYLBH2V3h3-Q1IoE86WXSOK0QQJ42Rscx49_fxvO9jP37Uwivds-AUj0Z3UNj9iH5UDT1QHwwmJpaqm26V0dqMXGkAw79WcbfBYtJukcYda79kRgl0HlQvykAKIr_jffTP4YneJGSQzi10c6xU0MbsjXJ6_BxTZUE-2ALl0Sxhs6JUKO35jjlH8d_g1aHwDAf55dNLZWDA08t_l3bGILc-03ctiTlo5EGR23IeQ5ikSQnmdoRKpQEbtX3YiW8mkQjVw7raAnF-az7i0fTSZQoItXQqxnlLjizGfXkptnIHUXn8Nju_6JiCH0i6UyKWE0AaPqwbH58IA6fsZeEYnf7vRt4uv-U6g1ofkJ4U-Co9dJEl60GwUJO4WeZG8178C_zfTv6b68nES4GI5Kt4F_Sa2uBVAXj4eh6oRBNoNNdipUfYwUCm2dxUzW4uwWSrv01-yP5VpzwriEz0-LgnvrmNESt4l3cZXvcvEmm-kIDIZeW-z4OKlM6IPB12GYQhkxaONhY_Ucci2UhCDWieZXnF0nnQINHo_L8M0zsl7ymiadcveominuzuudsBb499NG5btkTe5v751Lh4=w2400-h1600-no",
+  person2headline: "A proud millenial. Period. ",
+  file1: "anthony.html",
+  file2: "diane.html"
+};
+
+var lowerWestSide = {
+  name: "Lower West Side",
+  place1: "Little Italy: Enjoy a taste of Italy in this fun neighborhood! ",
+  place2: "This town has various instagrammable walls and hidden gems",
+  place3:
+    "National Museum of Mexican Art: Explore the hidden side of Mexican culture with unique art!",
+  person1name: "Nicole Xia",
+  person1imageURL:
+    "https://lh3.googleusercontent.com/JlpcEKVnzPB9DH4aiqHUoeLIklokpQwpjY1_YMioiRMFPIIq5sUvLwkfZtK7lJUgCfSlCPCTIU5RAlmX3f4KLO0Y7ILlWlxea_RfqFcQ1ixeAJ0_Ux3Mvog5gZVgMBgqmGo2kBu86yH4pBGUKN-8sUKT3yMba75yT-_pJdfQHBKZ4NhPNonVTRLQRAu0fIEd-Kx5Rj9mhPIDXB3ziDLbLGpcY-jzf7eOiUerzyrrpoMzWMCcco8E2P_akyyZqaie2ClSrbWkAe20pcYkw6Yb-A37yoPqwo7EwQxeBjAop2se2iQyBYimiRdprqyhjEKXCAu1hOEB_XVZM17cDwaFhIFE-6f0iFL2EDvniPTHej10FW2p-7L4go28WHRDQya2xeAvIoHDCehxINX3hOtx0lQY0CapSZP5tElnY3p_gIv1OtbHK0YCGRmol4EPS2xjWXhRCAx6heevsOd-ezNrazB0KSSASiDhsN9Icd9jpWkdF27F7x8vexcDUO9elKK0_65eOcEXj-5wd0r7_liBzt7NLea8LqVOC-6KQ5UKGE-AN8l1XjEJUDeUXn3uWQEOiwUv0oyZhKAthge3IvpCKkWgR2IfsVsx3lf-fZA=w2222-h1480-no",
+  person1headline: "Capturing the things I love, one moment at a time",
+  person2name: "Alice Liu",
+  person2imageURL:
+    "https://lh3.googleusercontent.com/__dVNNbj-1pKcQPAWD4UhoY_WamwVie-5IoH6TcCuyNytJag7l-5Tx-b2ozBHT34vd4hfQEXRFMyZTusIiBeBnriJKL5p7ejMQkJWgPxW9hiJAKzY4FWzSWOlzvyrx5dwKBjsyS-BRENY3ZRcUrgpDvfrKa6cfMc2yTAq7WllS0xDNPmztpaxcdTX91lOVUZktN22m1jURgs7jl0QjCgg-WzmLc_lsH1EJdsnZaMFdAcDfp_gZpkQvqjnjEI5Q6O8QDFWIIVVAqHaodme_EUR6FP1yl4ZWt1aW-mGI8YhYVx-d2pPqHFfA3R9rR01LxtOLz_M846Q_BG4XMiXxCNkoq-6HxMg-Wb67OEO4vO5FqtQv71DjEPY9zUGoyz-6EuJ4RaFPHRYiJwm-QHFEpmcI0Hee_vn5yNYPXKyC932s6quQTxFJer0Ue3rR1Z0iOwdiOfcnl9JUNjrgzCPoQU_Jj-gDOthYALFFgmviNUwTkwu3-2jzDUJdHi0GmcmLThPnczCOBHA3NZyBGtJQzebTMYZHhXnOakZpCtsx1DfPONrPCw-JzfVAStx8Md0ROmnBM5X3ORIKqCBxGohXunodaet1ayS439oZA9pOw=w2420-h1612-no",
+  person2headline:
+    "CS student at the morning, professional photographer at night",
+  file1: "nicole.html",
+  file2: "alice.html"
+};
+
+var wickerPark = {
+  name: "Wicker Park",
+  place1:
+    "The 606: This place is a perfect opportunity to take candids at the railway, park, and public art installations",
+  place2:
+    "Stan Donuts: This place features delicious donuts with a picture-friendly interior",
+  place3:
+    "Den Theatre: This space offers a relaxed environment in the cozy well-equipped lobby bar",
+  person1name: "Anthony Pham",
+  person1imageURL:
+    "https://lh3.googleusercontent.com/pcq9gpWUzRerodwEK7_4UkFWh4ghJzrKYcpaYw0rlUbiA97lMXBJJVPAE5uxdv-z9MnE_G4y5N2kKwc2F6iixyZRHOAn9gLhB2bO4lJ_k0oMFfSab81AMVyAEPBg9FmvP2qi_ppqP_nzV6cfC4-jBTztR1S5Wx8In3YA9E5xkUCOervC-_LJb36LJE-_QpUKraPGBzOFKEcI4slxXQGMLFvkJR8MHlCLdlqAhs9HK7L9X2CFKgHQLFJseDFGheoVmoMoTarOAyHTroGoCYUkFxUiWb9V21J82gOV7CKFe0ZQfBGvDK-P32Qk0Zpj-7Fe2qqDK78p9rd5Z3oDfZ4cgFTjiOmgTssqjbFZqd1Z9GqVRo0KlcElUdeM5lRqb90CYBzZdGYK3hpRHO6HDQ1ScXXb2oaU2Pft2UnnXBaCMkzxf83N5FspkLXr99LeCmGiPc6pm7A_webIpFUipX9DpdjmYMZG6gtubDYCQEDwxsR5NxB-3LSMoL7Y_k7Hs91bRjlTve_-1dsyUxdF6k6Bh7HyvZDl__qBbl2o5DHuxFfPulbDQyzEfrW02vj9uybleFUN5H3dMqJUfrxrwbPIgT4Mrz908mfBQOvNIlKnnW7uaDIUcCZlcuLfFm4PI1AGqQE6Wyz2sf6H7AFttHX73-AT3QwiqsvW=w2416-h1612-no",
+  person1headline: "I love my life just the way I capture my photos",
+  person2name: "Diane Wei",
+  person2imageURL:
+    "https://lh3.googleusercontent.com/Xe9ZsvjMyMhfoSqbJcbBq1v5NQoJusF4MvML-xWWM8u__NC1FRlZaY5g5mH2zGMrNR6HBv7r0bsc6MxPhNaYLBH2V3h3-Q1IoE86WXSOK0QQJ42Rscx49_fxvO9jP37Uwivds-AUj0Z3UNj9iH5UDT1QHwwmJpaqm26V0dqMXGkAw79WcbfBYtJukcYda79kRgl0HlQvykAKIr_jffTP4YneJGSQzi10c6xU0MbsjXJ6_BxTZUE-2ALl0Sxhs6JUKO35jjlH8d_g1aHwDAf55dNLZWDA08t_l3bGILc-03ctiTlo5EGR23IeQ5ikSQnmdoRKpQEbtX3YiW8mkQjVw7raAnF-az7i0fTSZQoItXQqxnlLjizGfXkptnIHUXn8Nju_6JiCH0i6UyKWE0AaPqwbH58IA6fsZeEYnf7vRt4uv-U6g1ofkJ4U-Co9dJEl60GwUJO4WeZG8178C_zfTv6b68nES4GI5Kt4F_Sa2uBVAXj4eh6oRBNoNNdipUfYwUCm2dxUzW4uwWSrv01-yP5VpzwriEz0-LgnvrmNESt4l3cZXvcvEmm-kIDIZeW-z4OKlM6IPB12GYQhkxaONhY_Ucci2UhCDWieZXnF0nnQINHo_L8M0zsl7ymiadcveominuzuudsBb499NG5btkTe5v751Lh4=w2400-h1600-no",
+  person2headline: "A proud millenial. Period. ",
+  file1: "anthony.html",
+  file2: "diane.html"
+};
+
+var places = [
+  nearNorthSide,
+  nearSouthSide,
+  chicagoLoop,
+  nearWestSide,
+  lowerWestSide,
+  wickerPark
+];
+
+function showDetails(index) {
+  document.getElementById("name").innerHTML = places[index].name;
+  document.getElementById("place1").innerHTML = places[index].place1;
+  document.getElementById("place2").innerHTML = places[index].place2;
+  document.getElementById("place3").innerHTML = places[index].place3;
+
+  document.getElementById("accordionList").style.display = "block";
+
+  document.getElementById("person1").innerHTML = places[index].person1name;
+  document.getElementById("headline1").innerHTML =
+    places[index].person1headline;
+  document.getElementById("picture1").src = places[index].person1imageURL;
+  document.getElementById("link1").href = places[index].file1;
+
+  document.getElementById("person2").innerHTML = places[index].person2name;
+  document.getElementById("headline2").innerHTML =
+    places[index].person2headline;
+  document.getElementById("picture2").src = places[index].person2imageURL;
+  document.getElementById("link2").href = places[index].file2;
+
+}
+
+(function($) {
+  var allPanels = $(".accordion > dd").hide();
+
+  $(".accordion > dt > a").click(function() {
+    allPanels.slideUp();
+    $(this)
+      .parent()
+      .next()
+      .slideDown();
+    return false;
+  });
+})(jQuery);
+
+$(document).ready(function() {
+  var $accordion = $(".accordion .accordion-item");
+  $accordion.click(function() {
+    var $this = $(this);
+    if ($this.hasClass("active")) {
+      $this
+        .removeClass("active")
+        .find("i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+    } else {
+      $accordion.removeClass("active");
+      $this.addClass("active");
+      $accordion
+        .find("i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+      $this.find("i").addClass("fa-minus");
+    }
+  });
+});
